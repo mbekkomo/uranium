cffaf234fd9ee548e498fc3d2043913bd9d6b416
Komo <afiqquraisyzulkarnain@gmail.com>
2025-02-09 18:47:59 +0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# uranium
+
+[![Package Version](https://img.shields.io/hexpm/v/uranium)](https://hex.pm/packages/uranium)
+[![Hex Docs](https://img.shields.io/badge/hex-docs-ffaff3)](https://hexdocs.pm/uranium/)
+
+```sh
+gleam add uranium@1
+```
+```gleam
+import uranium
+
+pub fn main() {
+  // TODO: An example of the project in use
+}
+```
+
+Further documentation can be found at <https://hexdocs.pm/uranium>.
+
+## Development
+
+```sh
+gleam run   # Run the project
+gleam test  # Run the tests
+```
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gleam
+          ] ++ (with beam27Packages; [
+            erlang
+            erlang-ls
+            rebar3
+          ]);
+        };
+      }
+    );
+}
